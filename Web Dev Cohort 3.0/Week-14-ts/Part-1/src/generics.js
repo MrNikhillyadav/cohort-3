f5e576c82fcfbbe02b89a77e833f717bd11f482b
Nikhil Yadav <facts.foundr@gmail.com>
2024-12-02 12:15:05 +0530

    add generics.ts

---
+++ Web Dev Cohort 3.0/Week-14-ts/Part-1/src/generics.js
@@ -0,0 +1,5 @@
+"use strict";
+function getFirstElement(arr) {
+    return arr[0];
+}
+const el = getFirstElement([1, "red", 3]);
---
+++ Web Dev Cohort 3.0/Week-14-ts/Part-1/src/generics.ts
@@ -0,0 +1,7 @@
+function getFirstElement<T>(arr: T[]) {
+          return arr[0];
+      }
+      
+
+      const element = getFirstElement(['red', 2 , 3]);
+      console.log('element: ', element);
