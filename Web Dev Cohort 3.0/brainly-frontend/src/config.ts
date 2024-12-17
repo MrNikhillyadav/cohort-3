7092d8d0b97704d4bbbd336f4cd3d8bfe0491f40
Nikhil Yadav <facts.foundr@gmail.com>
2024-12-17 12:48:18 +0530

    update

--- Web Dev Cohort 3.0/brainly-frontend/src/config.ts
+++ Web Dev Cohort 3.0/brainly-frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://cohort-3-six.vercel.app"
-// export const BACKEND_URL = "http://localhost:3000"
+// export const BACKEND_URL = "https://cohort-3-six.vercel.app"
+export const BACKEND_URL = "http://localhost:3000"
 
         
--- Web Dev Cohort 3.0/brainly/src/index.ts
+++ Web Dev Cohort 3.0/brainly/src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json())
 // app.use(cors())
 
 app.use(cors({
-    "origin": "https://brainly-frontend-peach.vercel.app",
+    "origin": "*", 
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
