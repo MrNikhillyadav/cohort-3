3013bc68e0608c8913857486bd586ba5b56f444f
Nikhil Yadav <facts.foundr@gmail.com>
2025-03-06 23:54:39 +0530

    add useEffect

--- Web Dev Cohort 3.0/react-hooks-practice/src/App.tsx
+++ Web Dev Cohort 3.0/react-hooks-practice/src/App.tsx
@@ -1,5 +1,5 @@
 // import UseStateHook from "./components/useStateHook";
-import Timer from "./components/timer";
+import Timer from "./components/useEffect";
 
 export default function App(){
   return <main  className="flex min-h-screen flex-col items-center justify-center p-24">
