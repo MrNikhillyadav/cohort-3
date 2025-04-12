e65505b7ca8c4ce3b05b5844c2fdf1d7a2ea8839
MrNikhillYadav <facts.foundr@gmail.com>
2025-04-12 23:27:36 +0530

    add apply method example

--- Web Dev Cohort 3.0/Build-Muscle-memory/ws/call.js
+++ Web Dev Cohort 3.0/Build-Muscle-memory/ws/call.js
@@ -1,10 +1,23 @@
+// const person = {
+//     name : "Nikhil Y",
+//     greet: function(){
+//         console.log( `Good morning Mr. ${this.name}`)
+//     } 
+// }
+
+// const person1 = { name : "Rajneesh"};
+
+// person.greet.call(person1)
+
+
 const person = {
-    name : "Nikhil Y",
-    greet: function(){
-        console.log( `Good morning Mr. ${this.name}`)
-    } 
-}
+    name: "Nikhil Y",
+    greet: function (greeting, punctuation) {
+        console.log(`${greeting} Mr. ${this.name}${punctuation}`);
+    }
+};
 
-const person1 = { name : "Rajneesh"};
+const person1 = { name: "Rajneesh" };
 
-person.greet.call(person1)
+person.greet.apply(person1, ["Good afternoon", "!"]);
+
