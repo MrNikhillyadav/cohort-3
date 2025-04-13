fb512786d4954514df0c53dd453e8f01629ec049
MrNikhillYadav <facts.foundr@gmail.com>
2025-04-13 15:12:10 +0530

    add Array.js

---
+++ Web Dev Cohort 3.0/Build-Muscle-memory/ws/Array.js
@@ -0,0 +1,13 @@
+// const fruits = new Array ("apple","banana","mango","guava");
+
+// for(let i = 0; i < fruits.length ; i++){
+//     console.log(fruits[i])
+// }
+
+
+
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+
+// array1.push(...array2)
+// console.log(array1); 
