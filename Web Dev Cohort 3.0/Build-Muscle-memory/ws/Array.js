973fdb3098e81b3b100df0da51af241510227ae4
MrNikhillYadav <facts.foundr@gmail.com>
2025-04-27 23:59:56 +0530

    add

--- Web Dev Cohort 3.0/Build-Muscle-memory/ws/Array.js
+++ Web Dev Cohort 3.0/Build-Muscle-memory/ws/Array.js
@@ -1,9 +1,13 @@
 // const fruits = new Array ("apple","banana","mango","guava");
+// const days = new Array ("monday","tuesday","wednesday","thursday", "friday", "saturday", "sunday");
 
 // for(let i = 0; i < fruits.length ; i++){
 //     console.log(fruits[i])
 // }
 
+// for(let i = 0; i < days.length ; i++){
+//     console.log(days[i])
+// }
 
 
 // const array1 = [1, 2, 3];
