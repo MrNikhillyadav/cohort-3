e26230549021fdeb7192a224be0a8fba915d7709
MrNikhillYadav <facts.foundr@gmail.com>
2025-06-11 13:51:33 +0530

    add imports

--- Web Dev Cohort 3.0/Build-Muscle-memory/react-basics/src/main.jsx
+++ Web Dev Cohort 3.0/Build-Muscle-memory/react-basics/src/main.jsx
@@ -1,10 +1,9 @@
+import './index.css'
+import App from './App.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {store } from '../src/app/store.js'
 import { Provider } from 'react-redux'
-
-import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <Provider store ={store}>
