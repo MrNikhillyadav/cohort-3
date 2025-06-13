d757a0e5646c91c78b9afaafb9490e6b305aeded
MrNikhillYadav <facts.foundr@gmail.com>
2025-06-13 23:13:15 +0530

    add

--- Web Dev Cohort 3.0/Build-Muscle-memory/ws/index.js
+++ Web Dev Cohort 3.0/Build-Muscle-memory/ws/index.js
@@ -2,6 +2,7 @@ const { WebSocket,WebSocketServer } = require("ws");
 
 const wss = new WebSocketServer({
     port : 8080
+
 })
 
 wss.on("connection", (socket) => {
@@ -23,5 +24,6 @@ wss.on("connection", (socket) => {
     socket.on("close",() => {
         console.log('connection closed!')
     })
+    
 })
 
