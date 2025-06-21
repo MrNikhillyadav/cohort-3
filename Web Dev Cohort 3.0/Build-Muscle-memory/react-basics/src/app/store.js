be03c2f69bfc85c237d48eff0f3bbca69008e8bd
MrNikhillYadav <facts.foundr@gmail.com>
2025-06-21 19:06:36 +0530

    updated store.js

--- Web Dev Cohort 3.0/Build-Muscle-memory/react-basics/src/app/store.js
+++ Web Dev Cohort 3.0/Build-Muscle-memory/react-basics/src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { colorSlice } from "../features/color/colorSlice";
+
 import { countSlice } from "../features/color/countSlice";
 
 export const store = configureStore({
