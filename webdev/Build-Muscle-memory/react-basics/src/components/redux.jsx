9ceb6ac46c83a0138bbcea4a6bd2c183a1372ae5
MrNikhillYadav <facts.foundr@gmail.com>
2025-06-23 22:59:12 +0530

    updateded

---
+++ webdev/Build-Muscle-memory/react-basics/src/components/redux.jsx
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../features/color/countSlice";
+import { changeColor } from "../features/color/colorSlice";
+
+export default function Redux() {
+    const count = useSelector((state) => state.count.value);
+    const color = useSelector((state) => state.color.value);
+    
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h2>Counter : {count}</h2>
+            <button onClick={() => dispatch(increment())}> + </button>
+            <button onClick={() => dispatch(decrement())}> - </button>
+
+            <br />
+
+            color is : <h2 style={{ color }}> {color} </h2>
+
+            <button onClick={() => dispatch(changeColor('blue'))}> blue </button>
+            <button onClick={() => dispatch(changeColor('orange'))}> orange </button>
+        </div>
+    );
+}
