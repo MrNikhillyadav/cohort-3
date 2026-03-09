f4ce435ce2e5410b5c38950b66fd400a3a52e183
Nikhil Y <nikyadav20032003@gmail.com>
2026-03-09 17:54:18 +0530

    get amount using  unpackAccount or getTokenAccountBalance

--- web3/week_08_PDA_bump/index.js
+++ web3/week_08_PDA_bump/index.js
@@ -15,11 +15,19 @@ async function getTokenBalance(owner, mintAddress) {
     false,
     TOKEN_2022_PROGRAM_ID
   );
-  console.log("ATA:", ataAddress.toBase58());
 
+  console.log("ATA:", ataAddress.toBase58());
+  
   const accountData = await connection.getAccountInfo(ataAddress);
   console.log("accountData: ", accountData);
 
+  // method : 1
+  const unpackedBalance = await unpackAccount(ataAddress,accountData, TOKEN_2022_PROGRAM_ID);
+  console.log("unpackedBalance : ", unpackedBalance.amount);  // 995000000000n
+  
+  // method : 2
+  const balance = await connection.getTokenAccountBalance(ataAddress);
+  console.log("Balance:", balance.value.uiAmount);   // 995
 }
 
 getTokenBalance(
