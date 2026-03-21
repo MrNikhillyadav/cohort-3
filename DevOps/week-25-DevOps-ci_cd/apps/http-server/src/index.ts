a2aefd0dde8b30fe0b9de98e0e9699286726d88c
Nikhil Y <nikyadav20032003@gmail.com>
2026-03-21 12:36:54 +0530

    add week-25-CI/CD

---
+++ DevOps/week-25-DevOps-ci_cd/apps/http-server/src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import {client} from '@repo/db/client'
+
+const app = express();
+app.use(express.json())
+
+app.get('/',(req,res)=> {
+    res.send('Hi there')
+})
+
+app.post('/signup',async (req,res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await client.user.create({
+                data : {
+                    username,
+                    password
+                }
+            })
+
+    res.json({
+        message : "Signup successful",
+        id : user.id
+
+    })
+})
+
+app.listen((4001),()=>{
+    console.log('listening on port 4001')
+})
