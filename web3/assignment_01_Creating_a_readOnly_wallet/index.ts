99631b05c2fa3b03d74304246655c6dc7385133e
Nikhil Y <nikyadav20032003@gmail.com>
2026-05-03 09:07:34 +0530

    add comment

--- web3/assignment_01_Creating_a_readOnly_wallet/index.ts
+++ web3/assignment_01_Creating_a_readOnly_wallet/index.ts
@@ -3,8 +3,8 @@ import { derivePath } from "ed25519-hd-key";
 import nacl from "tweetnacl";
 import { Keypair } from "@solana/web3.js";
 
-
-console.log("Mnemonic --> Seed --> DerivationPath --> Wallet Seeds")
+// steps
+console.log("Mnemonic --> Seed --> DerivationPath --> Wallet Seeds");
 
 const mnemonic =  generateMnemonic();
 console.log("mnemonic: ",mnemonic);
@@ -17,7 +17,7 @@ console.log("-----");
 function generateWalletKeyPairs(numOfAccounts:number = 4){
     for(let i=0; i < numOfAccounts ; i++){
 
-        const path = `m/44'/501'/${i}'/0'`;
+        const path = `m/44'/501'/${i}'/0'`; // 501 : solana coin type;
         const derivedWalletSeed =  derivePath(path, seed.toString("hex")).key;
 
         console.log(`derivedWalletSeed_${i}: ",${derivedWalletSeed.toString('hex')}`);
