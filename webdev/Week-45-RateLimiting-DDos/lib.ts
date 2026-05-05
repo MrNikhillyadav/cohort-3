897c1ea758ace252e451a54ccce1ca6b92500c69
Nikhil Y <nikyadav20032003@gmail.com>
2026-05-05 22:44:09 +0530

    fixed pad function

--- webdev/Week-45-RateLimiting-DDos/lib.ts
+++ webdev/Week-45-RateLimiting-DDos/lib.ts
@@ -1,17 +1,20 @@
 
-export function pad(num : number){
+export function pad(num : number){ // pad no. to make it six digits
     let numDigits = num == 0 ? 1 : 0;
     let numTemp = num;
+
     while(numTemp != 0){
         numTemp /= 10;
         numTemp = Math.floor(numTemp);
         numDigits++;
     }
+
     let ans = "";
 
-    for( let i =0; i < 6- numDigits; i++){
+    for( let i =0; i < 6 - numDigits; i++){
         ans += "0";
     }
+
     ans += num;
     return ans;
 }
