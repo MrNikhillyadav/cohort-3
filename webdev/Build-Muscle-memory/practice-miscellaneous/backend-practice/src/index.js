f341c43bd1529ea04ad35a7b98360160ec9e7d7f
Nikhil Y <nikyadav20032003@gmail.com>
2026-05-13 22:21:10 +0530

    fix route

--- webdev/Build-Muscle-memory/practice-miscellaneous/backend-practice/src/index.js
+++ webdev/Build-Muscle-memory/practice-miscellaneous/backend-practice/src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { UserModel } from './models/models';
+const PORT = 3000;
 
 const app = express();
 app.use(express.json())
 
-app.get('/',(req,res) => {
+app.get('/health',(req,res) => {
   res.status(200).json({
     message : "server is healthy"
   })
@@ -12,6 +13,6 @@ app.get('/',(req,res) => {
 
 app.use('/api/v1/users', userRouter);
 
-app.listen(3000,() => {
+app.listen(PORT,() => {
   console.log("server is running on port 3000");
 })
